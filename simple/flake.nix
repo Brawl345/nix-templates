9c151c1c6399b0cd7645ab8296edca43fd19c3b6
Andreas Bielawski <brawl@nyanya.de>
2023-06-11 11:42:51 +0200

    Fix template

--- simple/flake.nix
+++ simple/flake.nix
@@ -9,14 +9,17 @@
 
   outputs = { self, nixpkgs }:
     let
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ]
+          (system: function nixpkgs.legacyPackages.${system});
     in
     {
+
       devShells = forAllSystems
         (pkgs: {
           default = pkgs.mkShell {
@@ -27,5 +30,6 @@
             '';
           };
         });
+
     };
 }
